1f536c6fd136286aa04cac0b29fb22748a27b2a2
Aynekko <5f@mail.ru>
2023-08-13 12:39:03 +0300

    Make moving cables shake

--- client/render/tri.cpp
+++ client/render/tri.cpp
@@ -104,6 +104,11 @@ void R_DrawCable( cl_entity_t *e )
 	Vector vmidpoint;
 	
 	float TargetSway = e->curstate.fuser3;
+	
+	// origin of the cable can move when parented. if so, add shaking
+	float Speed = (e->curstate.origin - e->prevstate.origin).Length() / g_fFrametime;
+	TargetSway += Speed * 0.01;
+
 	// approach faster when shaking starts, but slower when coming to a halt
 	float ApproachSpeed = (e->curstate.fuser3 > tr.cableSwayIntensity[e->index]) ? g_fFrametime * (0.1f + 0.5f * fabs( tr.cableSwayIntensity[e->index] - e->curstate.fuser3 )) : (g_fFrametime * 0.1f);
 	tr.cableSwayIntensity[e->index] = CL_UTIL_Approach( TargetSway, tr.cableSwayIntensity[e->index], ApproachSpeed );
