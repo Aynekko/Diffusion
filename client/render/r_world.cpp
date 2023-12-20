d1312ec4fa50ad55971560de554e2c597664f5be
Aynekko <5f@mail.ru>
2023-12-20 12:22:09 +0300

    Water: fix incorrect brush bounds which were world origin dependant

--- client/render/r_world.cpp
+++ client/render/r_world.cpp
@@ -2951,7 +2951,7 @@ void R_DrawBrushList( void )
 
 				GL_Bind( GL_TEXTURE4, tr.waterTextures[(int)(tr.time * 20.0f) % WATER_TEXTURES] ); // u_NormalMap
 
-				float brushbounds = 2.0f * RadiusFromBounds( e->curstate.mins, e->curstate.maxs );
+				float brushbounds = 2.0f * (e->curstate.mins - e->curstate.maxs).Length();
 				pglUniform1fARB( RI->currentshader->u_zFar, -brushbounds );
 				pglUniform2fARB( RI->currentshader->u_ScreenSizeInv, screensizeinv.x, screensizeinv.y );
 			}
