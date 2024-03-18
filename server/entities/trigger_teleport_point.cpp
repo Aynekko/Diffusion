1358bc13d222658d75c76140847de86f18f123ae
Aynekko <5f@mail.ru>
2024-03-18 10:41:01 +0300

    trigger_teleport_point: add "remove on fire" flag

--- server/entities/trigger_teleport_point.cpp
+++ server/entities/trigger_teleport_point.cpp
@@ -10,6 +10,7 @@
 
 #define PT_KEEP_VELOCITY	BIT(0) // keep the velocity of the entity
 #define PT_KEEP_ANGLES		BIT(1) // don't snap to new angles
+#define PT_REMOVEONFIRE		BIT(2)
 
 // UNDONE: redirect player's velocity to entity's view origin?
 
@@ -83,4 +84,7 @@ void CTriggerTeleportPoint::Use(CBaseEntity* pActivator, CBaseEntity* pCaller, U
 	pEntity->Teleport(&TargetOrg, &EntityAngles, &EntityVelocity);
 
 	UTIL_FireTargets( pev->target, pActivator, pCaller, USE_TOGGLE, 0 );
+
+	if( HasSpawnFlags( PT_REMOVEONFIRE ) )
+		UTIL_Remove( this );
 }
