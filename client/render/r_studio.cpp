7a697f3b999fd3bffc2810172f3a494c637e0496
Aynekko <5f@mail.ru>
2024-03-24 18:17:10 +0300

    Disable fog for additive studio models (forgot)

--- client/render/r_studio.cpp
+++ client/render/r_studio.cpp
@@ -5258,7 +5258,10 @@ void CStudioModelRenderer::DrawStudioMeshes( void )
 			// view right
 			studio_params[1] = Vector4D( right.x, right.y, right.z, 0.0f );
 			// fog params
-			studio_params[2] = Vector4D( tr.fogColor[0], tr.fogColor[1], tr.fogColor[2], tr.fogDensity );
+			if( m_pCurrentEntity->curstate.rendermode == kRenderTransAdd || m_pCurrentEntity->curstate.renderfx == kRenderFxFullbright || m_pCurrentEntity->curstate.renderfx == kRenderFxFullbrightNoShadows )
+				studio_params[2] = Vector4D( 0.0f, 0.0f, 0.0f, 0.0f ); // disable fog
+			else
+				studio_params[2] = Vector4D( tr.fogColor[0], tr.fogColor[1], tr.fogColor[2], tr.fogDensity );
 			pglUniform4fvARB( RI->currentshader->u_StudioParams, 3, &studio_params[0][0] );
 
 			if( FBitSet( m_pModelInstance->info_flags, MF_VERTEX_LIGHTING ) )
