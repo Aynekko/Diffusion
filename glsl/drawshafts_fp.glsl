74f79191f171d87237bad4034774eaba5ea3e922
Aynekko <5f@mail.ru>
2024-09-07 19:06:30 +0300

    Micro-optimize shaders - drawshafts and enhance

--- glsl/drawshafts_fp.glsl
+++ glsl/drawshafts_fp.glsl
@@ -50,6 +50,8 @@ void main( void )
 	float fShaftsMask = saturate( 1.00001 - accum.w );
 	float fBlend = accum.w * Brightness;	// shaft brightness
 
+	/*
+	// original
 	const int Steps = 32;
 	const float step = 8.0 / Steps;
 	for( int i = 1; i <= Steps; i++ )
@@ -57,6 +59,38 @@ void main( void )
 		accum += texture2D( u_ColorMap, tc + sunVec.xy * (0.5 + step * i) ) * (1.0 - (0.5 + step * i) / 8.0);
 	}
 	accum /= Steps;
+	*/
+
+	// 28 steps (unrolled, optimized)
+	accum += texture2D( u_ColorMap, sunVec.xy * 0.75 + tc ) * 0.90625;
+	accum += texture2D( u_ColorMap, sunVec.xy * 1.00 + tc ) * 0.87500;
+	accum += texture2D( u_ColorMap, sunVec.xy * 1.25 + tc ) * 0.84375;
+	accum += texture2D( u_ColorMap, sunVec.xy * 1.50 + tc ) * 0.81250;
+	accum += texture2D( u_ColorMap, sunVec.xy * 1.75 + tc ) * 0.78125;
+	accum += texture2D( u_ColorMap, sunVec.xy * 2.00 + tc ) * 0.75000;
+	accum += texture2D( u_ColorMap, sunVec.xy * 2.25 + tc ) * 0.71875;
+	accum += texture2D( u_ColorMap, sunVec.xy * 2.50 + tc ) * 0.68750;
+	accum += texture2D( u_ColorMap, sunVec.xy * 2.75 + tc ) * 0.65625;
+	accum += texture2D( u_ColorMap, sunVec.xy * 3.00 + tc ) * 0.62500;
+	accum += texture2D( u_ColorMap, sunVec.xy * 3.25 + tc ) * 0.59375;
+	accum += texture2D( u_ColorMap, sunVec.xy * 3.50 + tc ) * 0.56250;
+	accum += texture2D( u_ColorMap, sunVec.xy * 3.75 + tc ) * 0.53125;
+	accum += texture2D( u_ColorMap, sunVec.xy * 4.00 + tc ) * 0.50000;
+	accum += texture2D( u_ColorMap, sunVec.xy * 4.25 + tc ) * 0.46875;
+	accum += texture2D( u_ColorMap, sunVec.xy * 4.50 + tc ) * 0.43750;
+	accum += texture2D( u_ColorMap, sunVec.xy * 4.75 + tc ) * 0.40625;
+	accum += texture2D( u_ColorMap, sunVec.xy * 5.00 + tc ) * 0.37500;
+	accum += texture2D( u_ColorMap, sunVec.xy * 5.25 + tc ) * 0.34375;
+	accum += texture2D( u_ColorMap, sunVec.xy * 5.50 + tc ) * 0.31250;
+	accum += texture2D( u_ColorMap, sunVec.xy * 5.75 + tc ) * 0.28125;
+	accum += texture2D( u_ColorMap, sunVec.xy * 6.00 + tc ) * 0.25000;
+	accum += texture2D( u_ColorMap, sunVec.xy * 6.25 + tc ) * 0.21875;
+	accum += texture2D( u_ColorMap, sunVec.xy * 6.50 + tc ) * 0.18750;
+	accum += texture2D( u_ColorMap, sunVec.xy * 6.75 + tc ) * 0.15625;
+	accum += texture2D( u_ColorMap, sunVec.xy * 7.00 + tc ) * 0.12500;
+	accum += texture2D( u_ColorMap, sunVec.xy * 7.25 + tc ) * 0.09375;
+	accum += texture2D( u_ColorMap, sunVec.xy * 7.50 + tc ) * 0.06250;
+	accum /= 28;
 
  	accum *= 2.0 * vec4( sunDist, sunDist, sunDist, 1.0 );
 	
--- glsl/enhance_fp.glsl
+++ glsl/enhance_fp.glsl
@@ -7,28 +7,27 @@ uniform vec2        u_ScreenSizeInv;
 
 varying vec2        var_TexCoord;
 
+#define SATURATE
+#define SHARPEN
+
 void main( void )
 {
 	vec4 color = texture2D( u_ColorMap, var_TexCoord, 0.0 );
-	const bool Saturate = true;
-	const bool Sharpen = true;
 	
 	// saturate
-	if( Saturate )
-	{
-		const float Saturation = 0.55f;
-		vec3 col_hsv = RGBtoHSV( color.rgb );
-		col_hsv.y *= ( Saturation * 2.0 );
-		vec3 col_rgb = HSVtoRGB( col_hsv.rgb );
-		color.rgb = col_rgb;
-	}
+#if defined( SATURATE )
+	const float Saturation = 0.55f;
+	vec3 col_hsv = RGBtoHSV( color.rgb );
+	col_hsv.y *= ( Saturation * 2.0 );
+	vec3 col_rgb = HSVtoRGB( col_hsv.rgb );
+	color.rgb = col_rgb;
+#endif
 
 	// sharpen
-	if( Sharpen )
-	{
-		vec4 sharpen = sharpen( u_ColorMap, var_TexCoord, u_ScreenSizeInv );
-		color.rgb = mix( color.rgb, sharpen.rgb, 0.2 );
-	}
+#if defined( SHARPEN )
+	vec4 sharpen = sharpen( u_ColorMap, var_TexCoord, u_ScreenSizeInv );
+	color.rgb = mix( color.rgb, sharpen.rgb, 0.2 );
+#endif
 
 	gl_FragColor = color;
 }
