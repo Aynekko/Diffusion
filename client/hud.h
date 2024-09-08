Aynekko/Diffusion

commit 0d1f4e0f58be4ea3fb83f8c628fb0421140c290a

Major optimization for dynamic lights - Building a single light list at the beginning of a frame to prevent lots of unnecessary culling (checkboxvisible is very expensive)

client/render/r_light.cpp
@@ -409,13 +409,41 @@ int R_CountPlights( bool countShadowLights )
 		if( pl->die < tr.time || !pl->radius || pl->culled )
 			continue;
 
-		if( (FBitSet( pl->flags, CF_NOSHADOWS )) && countShadowLights )
+		if( countShadowLights && FBitSet( pl->flags, CF_NOSHADOWS ) )
 			continue;
 
 		numPlights++;
 	}
 	
 	return numPlights;
+}
+
+//====================================================================================
+// R_BuildLightList: build a list of available lights for current renderpass
+//====================================================================================
+void R_BuildLightList( void )
+{
+	tr.num_dynlights = 0;
+	
+	if( !R_CountPlights() )
+		return;
+
+	plight_t *pl = cl_plights;
+
+	for( int i = 0; i < MAX_PLIGHTS; i++, pl++ )
+	{
+		if( pl->die < tr.time || !pl->radius || pl->culled )
+			continue;
+
+		if( !Mod_CheckBoxVisible( pl->absmin, pl->absmax ) )
+			continue;
+
+		if( R_CullBox( pl->absmin, pl->absmax ) )
+			continue;
+
+		tr.cur_dynlights[tr.num_dynlights] = pl;
+		tr.num_dynlights++;
+	}
 }
 
 /*
@@ -477,6 +505,8 @@ void R_PushDlights( void )
 		else
 			r_stats.c_plights++;
 	}
+
+	R_BuildLightList();
 }
 
 /*
@@ -577,13 +607,15 @@ Vector R_LightsForPoint( const Vector &point, float radius )
 
 	lightColor = g_vecZero;
 
-	for( int lnum = 0; lnum < MAX_PLIGHTS; lnum++ )
-	{
-		plight_t *pl = &cl_plights[lnum];
-		float atten = 1.0f;
+	if( !tr.num_dynlights )
+		return lightColor;
 
-		if( pl->die < tr.time || !pl->radius )
-			continue;
+	plight_t *pl = NULL;
+
+	for( int lnum = 0; lnum < tr.num_dynlights; lnum++ )
+	{
+		pl = tr.cur_dynlights[lnum];
+		float atten = 1.0f;
 
 		Vector dir = (pl->origin - point);
 		float dist = dir.Length();

client/render/r_local.h
@@ -366,6 +366,8 @@ typedef struct
 	int	num_solid_entities;
 	int	num_trans_entities;
 	int num_subview_entities;
+	plight_t *cur_dynlights[MAX_PLIGHTS]; // list of dynlights in the current pass
+	int num_dynlights; // number of lights
 
 	gl_bmodelface_t	draw_surfaces[MAX_MAP_FACES];		// 390 kB here
 	int		num_draw_surfaces;
@@ -468,6 +470,8 @@ typedef struct
 	int nullmodelindex;
 
 	int volumetric_light_texture;
+
+	int test; // counter/bool for testing purposes
 } ref_globals_t;
 
 typedef struct
@@ -748,6 +752,7 @@ void DrawDecalsBatch( void );
 //
 void R_AnimateLight( void );
 void R_PushDlights( void );
+void R_BuildLightList( void );
 int R_CountPlights( bool countShadowLights = false );
 Vector R_LightsForPoint( const Vector &point, float radius );
 void R_GetLightVectors( cl_entity_t *pEnt, Vector &origin, Vector &angles );

client/render/r_shadows.cpp
@@ -571,14 +571,18 @@ void R_RenderShadowCubeSide( plight_t *pl, int side )
 
 void R_RenderShadowmaps(void)
 {
+	if( !tr.num_dynlights )
+		return;
+	
 	if (R_FullBright() || !CVAR_TO_BOOL(r_shadows) || tr.fGamePaused || tr.shadows_notsupport)
 		return;
 
 	if( IsBuildingCubemaps() )
 		return;
 	
-	// check for dynamic lights
-	if (!R_CountPlights(true)) return;
+	// check for dynamic lights (exclude those with "no shadows" set)
+	if( !R_CountPlights( true ) ) 
+		return;
 
 	CurrentPassFrustum = RI->frustum;
 
@@ -590,20 +594,13 @@ void R_RenderShadowmaps(void)
 		R_ResetShadowTextures();
 	}
 
-	for (int i = 0; i < MAX_PLIGHTS; i++)
+	plight_t *pl = NULL;
+
+	for (int i = 0; i < tr.num_dynlights; i++)
 	{
-		plight_t* pl = &cl_plights[i];
+		pl = tr.cur_dynlights[i];
 
 		if( FBitSet( pl->flags, CF_NOSHADOWS ) )
-			continue;
-
-		if (pl->die < tr.time || !pl->radius || pl->culled)
-			continue;
-
-		if( !Mod_CheckBoxVisible( pl->absmin, pl->absmax ) )
-			continue;
-
-		if( R_CullBox( pl->absmin, pl->absmax ) )
 			continue;
 
 		if( pl->pointlight && tr.omni_shadows_notsupport )

client/render/r_studio.cpp
@@ -5208,25 +5208,18 @@ void CStudioModelRenderer::RenderDynLightList( void )
 	if( IsBuildingCubemaps() )
 		return;
 
-	if( !R_CountPlights() )
+	if( !tr.num_dynlights )
 		return;
 
 	GL_Blend( GL_TRUE );
 	GL_DepthMask( GL_FALSE );
 	GL_BlendFunc( GL_ONE, GL_ONE );
 	pglEnable( GL_SCISSOR_TEST );
-	plight_t *pl = cl_plights;
+	plight_t *pl = NULL;
 
-	for( int i = 0; i < MAX_PLIGHTS; i++, pl++ )
+	for( int i = 0; i < tr.num_dynlights; i++ )
 	{
-		if( pl->die < tr.time || !pl->radius || pl->culled )
-			continue;
-
-		if( !Mod_CheckBoxVisible( pl->absmin, pl->absmax ) )
-			continue;
-
-		if( R_CullBox( pl->absmin, pl->absmax ) )
-			continue;
+		pl = tr.cur_dynlights[i];
 
 		RI->currentlight = pl;
 

client/render/r_world.cpp
@@ -2573,7 +2573,7 @@ void R_RenderDynLightList( void )
 	if( R_FullBright() )
 		return;
 
-	if( !R_CountPlights() )
+	if( !tr.num_dynlights )
 		return;
 
 	GL_Blend( GL_TRUE );
@@ -2581,20 +2581,13 @@ void R_RenderDynLightList( void )
 	pglEnable( GL_SCISSOR_TEST );
 	pglBindVertexArray( world->vertex_array_object );
 
-	plight_t *pl = cl_plights;
+	plight_t *pl = NULL;
 
-	for( int i = 0; i < MAX_PLIGHTS; i++, pl++ )
+	for( int i = 0; i < tr.num_dynlights; i++ )
 	{
-		if( pl->die < tr.time || !pl->radius || pl->culled )
-			continue;
+		pl = tr.cur_dynlights[i];
 
 		RI->currentlight = pl;
-
-		if( !Mod_CheckBoxVisible( pl->absmin, pl->absmax ) )
-			continue;
-
-		if( R_CullBox( pl->absmin, pl->absmax ) )
-			continue;
 
 		// draw world from light position
 		R_BuildFaceListForLight( pl );

commit f4952f9f5ed385768cfd3ca25168ce02a6da2cb4

Big maintenance - fix flickering shadows when there's a few lights. Shadowpass still requires separate light culling for some reason (bug introduced in 0d1f4e0f58be4ea3fb83f8c628fb0421140c290a) - r_speeds: add vis mergers counter (entities with EF_MERGE_VISIBILITY effect) - lens flare: modify lensflare visibility properly, according to weather intensity - particles: very rare depthmask issue, possibly fixed, didn't catch it again yet - r_studio: StudioStaticLight is required for shadowpass after all (bug introduced in 4a0f330a5c53cb290d8b8351aa0e7e6824422e97) - generate_exposure shader: adjustment again - func_monitor: possibly fixed multiplayer issue. FIND_CLIENT_IN_PVS doesn't seem to work as intended, so I chose another solution

client/hud.h
@@ -932,6 +932,7 @@ public:
 	bool ScreenDrips_Visible;
 	float ScreenDrips_DripIntensity;
 	float ScreenDrips_OverrideTime;
+	float Weather_Intensity;
 
 	// viewmodel lag
 	Vector m_vecLastFacing;

client/render/r_light.cpp
@@ -432,11 +432,11 @@ void R_BuildLightList( void )
 
 	for( int i = 0; i < MAX_PLIGHTS; i++, pl++ )
 	{
+		if( pl->die < tr.time || !pl->radius || pl->culled )
+			continue;
+
 		if( !UTIL_IsLocal( pl->entindex ) ) // do not perform culling for this player's flashlight - it's always visible
 		{
-			if( pl->die < tr.time || !pl->radius || pl->culled )
-				continue;
-
 			if( !Mod_CheckBoxVisible( pl->absmin, pl->absmax ) )
 				continue;
 

client/render/r_local.h
@@ -489,6 +489,7 @@ typedef struct
 	unsigned int	c_particles_total;
 	unsigned int	c_particles_culled;
 	unsigned int	c_decals;
+	unsigned int	c_vis_mergers; // EF_MERGE_VISIBILITY ent count
 
 	unsigned int	c_portal_passes;
 	unsigned int	c_mirror_passes;

client/render/r_main.cpp
@@ -1927,6 +1927,7 @@ void HUD_PrintStats( void )
 		R_Speeds_Printf( "%3i screens\n", r_stats.c_screen_passes );
 		R_Speeds_Printf( "%3i shadows\n", r_stats.c_shadow_passes );
 		R_Speeds_Printf( "%3i 3d sky\n", r_stats.c_sky_passes );
+		R_Speeds_Printf( "%3i vis mergers\n", r_stats.c_vis_mergers );
 		R_Speeds_Printf( "%3i total\n\n", r_stats.num_passes );
 		R_Speeds_Printf( "Dynamic lights:\n" );
 		R_Speeds_Printf( "%3i total (%3i mdl | %3i surf)\n\n", r_stats.c_plights, r_stats.c_plights_meshes, r_stats.num_light_surfaces );
@@ -2225,7 +2226,7 @@ int HUD_RenderFrame( const ref_viewpass_t *rvp )
 	R_UnloadFarGrass();
 
 	HUD_PrintStats();
-	
+
 	return 1;
 }
 

client/render/r_misc.cpp
@@ -289,6 +289,9 @@ int HUD_AddEntity( int type, struct cl_entity_s* ent, const char* modelname )
 		if( gl_renderer->value == 0 )
 			return 1;
 
+		if( ent->curstate.effects & EF_MERGE_VISIBILITY )
+			r_stats.c_vis_mergers++;
+
 		if( ent->curstate.effects & EF_SCREEN )
 		{
 			ent->curstate.movetype = 0; // clear movetype for screen (engine sets this to MOVETYPE_FOLLOW because we use aiment. Argh!)

client/render/r_postprocess.cpp
@@ -1069,6 +1069,9 @@ void LensFlare( void )
 	if( !tr.bSkySurfFound )
 		return; // no sky surface was found this frame, don't waste time tracing
 
+	if( gHUD.Weather_Intensity >= 0.99f )
+		return; // complete storm (rain/snow), no lensflare for you!
+
 	Vector sunangles, sundir, suntarget;
 	Vector forward, right, up, screen;
 	Vector v_angles, v_origin;
@@ -1129,7 +1132,7 @@ void LensFlare( void )
 	else
 		gHUD.LensFlareAlpha -= 10.0f * g_fFrametime;
 
-	gHUD.LensFlareAlpha = bound( 0.0f, gHUD.LensFlareAlpha, 1.0f - gHUD.ScreenDrips_DripIntensity );
+	gHUD.LensFlareAlpha = bound( 0.0f, gHUD.LensFlareAlpha, 1.0f );
 
 	if( gHUD.LensFlareAlpha <= 0.01f )
 		return;
@@ -1148,7 +1151,7 @@ void LensFlare( void )
 	ASSERT( RI->currentshader != NULL );
 
 	pglUniform2fARB( RI->currentshader->u_ScreenSizeInv, (float)(glState.width), (float)(glState.height) );
-	pglUniform1fARB( RI->currentshader->u_Accum, gHUD.LensFlareAlpha * pow( DotP, 10 ) );
+	pglUniform1fARB( RI->currentshader->u_Accum, gHUD.LensFlareAlpha * pow( DotP, 10 ) * (1.0f - gHUD.Weather_Intensity) );
 	pglUniform2fARB( RI->currentshader->u_LightOrigin, view.x, view.y );
 
 	RenderFSQ( glState.width, glState.height );

client/render/r_quakeparticle.cpp
@@ -608,8 +608,6 @@ void CQuakePartSystem::DrawParticles( MemBlock<CQuakePart> &ParticleArray )
 		curParticle = ParticleArray.GetCurrent();
 	}
 
-	GL_Blend( GL_TRUE );
-
 	if( glState.drawTrans )
 	{
 		if( UseAdditive )
@@ -646,8 +644,6 @@ void CQuakePartSystem::DrawParticles( MemBlock<CQuakePart> &ParticleArray )
 		GL_AlphaTest( GL_FALSE );
 		GL_AlphaFunc( GL_GREATER, DEFAULT_ALPHATEST );
 	}
-
-	GL_Blend( GL_FALSE );
 }
 
 bool CQuakePart::Evaluate( float gravity )
@@ -1034,8 +1030,6 @@ bool CQuakePart::Evaluate( float gravity )
 	for( int i = 0; i < 4; i++ )
 		AddPointToBounds( verts[i], absmin, absmax );
 
-	GL_Blend( GL_TRUE );
-
 	if( FBitSet( m_iFlags, FPART_TWOSIDE ) || (FBitSet( m_iFlags, FPART_AFLOAT ) && FBitSet( m_iFlags, FPART_FLOATING_ORIENTED )) ) // visible from underwater
 		GL_Cull( GL_NONE );
 	else
@@ -1083,8 +1077,6 @@ bool CQuakePart::Evaluate( float gravity )
 		GL_AlphaTest( GL_FALSE );
 		GL_AlphaFunc( GL_GREATER, DEFAULT_ALPHATEST );
 	}
-
-	GL_Blend( GL_FALSE );
 
 	return true;
 }
@@ -1675,8 +1667,12 @@ void CQuakePartSystem :: Update( void )
 
 	if( glState.drawTrans )
 		GL_DepthMask( GL_FALSE );
+	else
+		GL_DepthMask( GL_TRUE );
 
 	pglBindVertexArray( GL_FALSE ); // just in case
+
+	GL_Blend( GL_TRUE );
 
 	DrawParticles( ParticleArray_Default );
 	DrawParticles( ParticleArray_Dustmote );
@@ -1727,6 +1723,10 @@ void CQuakePartSystem :: Update( void )
 	m_pActiveParticles = pActive;
 
 	r_stats.c_particles_total = partcounter;
+
+	GL_Blend( GL_FALSE );
+	if( glState.drawTrans )
+		GL_DepthMask( GL_TRUE );
 
 //	ExplosionOrg = g_vecZero;
 }

client/render/r_shadows.cpp
@@ -570,10 +570,7 @@ void R_RenderShadowCubeSide( plight_t *pl, int side )
 }
 
 void R_RenderShadowmaps(void)
-{
-	if( !tr.num_dynlights )
-		return;
-	
+{	
 	if (R_FullBright() || !CVAR_TO_BOOL(r_shadows) || tr.fGamePaused || tr.shadows_notsupport)
 		return;
 
@@ -594,14 +591,24 @@ void R_RenderShadowmaps(void)
 		R_ResetShadowTextures();
 	}
 
-	plight_t *pl = NULL;
-
-	for (int i = 0; i < tr.num_dynlights; i++)
+	for( int i = 0; i < MAX_PLIGHTS; i++ )
 	{
-		pl = tr.cur_dynlights[i];
+		plight_t *pl = &cl_plights[i];
 
 		if( FBitSet( pl->flags, CF_NOSHADOWS ) )
 			continue;
+
+		if( pl->die < tr.time || !pl->radius || pl->culled )
+			continue;
+
+		if( !UTIL_IsLocal( pl->entindex ) ) // do not perform culling for this player's flashlight - it's always visible
+		{
+			if( !Mod_CheckBoxVisible( pl->absmin, pl->absmax ) )
+				continue;
+
+			if( R_CullBox( pl->absmin, pl->absmax ) )
+				continue;
+		}
 
 		if( pl->pointlight && tr.omni_shadows_notsupport )
 			continue;

client/render/r_studio.cpp
@@ -3849,9 +3849,6 @@ StudioStaticLight
 */
 void CStudioModelRenderer::StudioStaticLight( cl_entity_t *ent )
 {
-	if( RI->params & RP_SHADOWPASS )
-		return;
-	
 	// setup advanced vertexlighting for env_static entities
 	if( (ent->curstate.iuser3 > 0) && world->vertex_lighting != NULL )
 	{
@@ -4182,6 +4179,8 @@ void CStudioModelRenderer::StudioDrawShell( void )
 
 		m_pStudioHeader = (studiohdr_t *)IEngineStudio.Mod_Extradata( m_pRenderModel );
 	}
+
+	IEngineStudio.GL_SetRenderMode( kRenderNormal );
 }
 
 /*

client/render/r_weather.cpp
@@ -423,6 +423,7 @@ void R_InitWeather( void )
 	gHUD.ScreenDrips_CurVisibility = 0.0f;
 	gHUD.ScreenDrips_DripIntensity = 0.0f;
 	gHUD.ScreenDrips_OverrideTime = 0.0f;
+	gHUD.Weather_Intensity = 0.0f;
 
 	RainTexture = LOAD_TEXTURE( "gfx/particles/raindrop", NULL, 0, 0 );
 }
@@ -890,12 +891,12 @@ void R_DrawWeather( void )
 	ProcessRain();
 	ProcessFXObjects();
 
+	// a neat global to determine the intensity of a storm if needed
+	gHUD.Weather_Intensity = bound( 0.0f, Rain.dripsPerSecond / 2500.0f, 1.0f );  // this number can be tuned
+
 	gHUD.ScreenDrips_Visible = false;
 	if( gHUD.ScreenDrips_OverrideTime < tr.time )
-	{
-		gHUD.ScreenDrips_DripIntensity = Rain.dripsPerSecond / 2500.0f; // this number can be tuned
-		gHUD.ScreenDrips_DripIntensity = bound( 0.0f, gHUD.ScreenDrips_DripIntensity, 0.5f );
-	}
+		gHUD.ScreenDrips_DripIntensity = bound( 0.0f, gHUD.Weather_Intensity, 0.5f ); // for water drops shader
 
 	if( Rain.dripsPerSecond > 0 )
 	{

client/utils.cpp
@@ -698,7 +698,7 @@ R_ScissorForBounds
 */
 static bool R_ScissorForBounds( const Vector bbox[8], float *x, float *y, float *w, float *h )
 {
-	static int	cornerIndices[6][4] = {{3, 2, 6, 7}, {0, 1, 5, 4}, {2, 3, 1, 0}, {4, 5, 7, 6}, {1, 3, 7, 5}, {2, 0, 4, 6}};
+	const int	cornerIndices[6][4] = {{3, 2, 6, 7}, {0, 1, 5, 4}, {2, 3, 1, 0}, {4, 5, 7, 6}, {1, 3, 7, 5}, {2, 0, 4, 6}};
 	Vector		points[2][MAX_POLYGON_POINTS];
 	float		ix1, iy1, ix2, iy2;
 	float		x1, y1, x2, y2;

glsl/generate_exposure_fp.glsl
@@ -33,8 +33,8 @@ float ConvertEV100ToExposure( float ev100 )
 
 void main()
 {
-    const float exposureMin = 0.5;
-    const float exposureMax = 0.8;
+    const float exposureMin = 0.6;
+    const float exposureMax = 0.9;
     const float exposureScale = 1.0;
     const float adaptRateToDark = 1.2;
     const float adaptRateToBright = 1.6;

server/enginecallback.h
@@ -113,7 +113,7 @@ inline void *GET_PRIVATE( edict_t *pent )
 #define FIND_ENTITY_BY_STRING	(*g_engfuncs.pfnFindEntityByString)
 #define GETENTITYILLUM	(*g_engfuncs.pfnGetEntityIllum)
 #define FIND_ENTITY_IN_SPHERE		(*g_engfuncs.pfnFindEntityInSphere)
-#define FIND_CLIENT_IN_PVS			(*g_engfuncs.pfnFindClientInPVS)
+#define FIND_CLIENT_IN_PVS			(*g_engfuncs.pfnFindClientInPVS) // cycles through clients on consecutive calls
 #define EMIT_AMBIENT_SOUND			(*g_engfuncs.pfnEmitAmbientSound)
 #define GET_MODEL_PTR				(*g_engfuncs.pfnGetModelPtr)
 #define REG_USER_MSG				(*g_engfuncs.pfnRegUserMsg)

server/entities/func_monitor.cpp
@@ -234,23 +234,61 @@ void CFuncMonitor :: VisThink( void )
 		// no reason to find in PVS itself
 		if( AllowToFindClientInPVS( pTarget ))
 		{
-			CBaseEntity *pClient = CBaseEntity::Instance( FIND_CLIENT_IN_PVS( edict() ) );
-			
-			if( FNullEnt( pClient ) )
-				ClearBits( pTarget->pev->effects, EF_MERGE_VISIBILITY );
-			else
+			CBaseEntity *pClient = NULL;
+
+			// special case for multiplayer - we need to check all clients
+			if( gpGlobals->maxClients > 1 )
 			{
-				// diffusion - also check distance!
-				// doesn't work in multiplayer
-				if( (gpGlobals->maxClients == 1) && (pev->iuser4 > 0) )
+				// problem! FIND_CLIENT_IN_PVS produces bollocks - no matter how many times I cycle through, the loop
+				// told me that no player's were found in the screen's pvs. And occasionally is DOES find them.
+				// the screen keeps turning on and off, so...just keep it running, only make a special case if there's a fadedistance.
+				// (and there's better be one because visibility merging is damn expensive)
+				
+				// by default set it as visible
+				SetBits( pTarget->pev->effects, EF_MERGE_VISIBILITY );
+				// in case of the screen having fadedistance, check if all clients are far enough to disable the screen
+				if( pev->iuser4 > 0 )
 				{
-					if( (pClient->GetAbsOrigin() - GetAbsOrigin()).Length() > (pev->iuser4 + 255) )
+					bool ClosePlayerFound = false;
+					for( int i = 1; i <= gpGlobals->maxClients; i++ )
+					{
+						CBaseEntity *pPlayer = UTIL_PlayerByIndex( i );
+
+						if( !pPlayer || pPlayer->HasFlag( F_BOT ) )
+							continue;
+
+						if( (pPlayer->GetAbsOrigin() - GetAbsOrigin()).Length() < (pev->iuser4 + 255) )
+						{
+							// at least one real player is close enough. keep screen running
+							ClosePlayerFound = true;
+							break;
+						}
+					}
+
+					if( !ClosePlayerFound )
 						ClearBits( pTarget->pev->effects, EF_MERGE_VISIBILITY );
+				}
+			}
+			else // single player
+			{
+				pClient = CBaseEntity::Instance( FIND_CLIENT_IN_PVS( edict() ) );
+
+				if( FNullEnt( pClient ) )
+					ClearBits( pTarget->pev->effects, EF_MERGE_VISIBILITY );
+				else
+				{
+					// diffusion - also check distance!
+					// doesn't work in multiplayer
+					if( pev->iuser4 > 0 )
+					{
+						if( (pClient->GetAbsOrigin() - GetAbsOrigin()).Length() > (pev->iuser4 + 255) )
+							ClearBits( pTarget->pev->effects, EF_MERGE_VISIBILITY );
+						else
+							SetBits( pTarget->pev->effects, EF_MERGE_VISIBILITY );
+					}
 					else
 						SetBits( pTarget->pev->effects, EF_MERGE_VISIBILITY );
 				}
-				else
-					SetBits( pTarget->pev->effects, EF_MERGE_VISIBILITY );
 			}
 		}
 	}