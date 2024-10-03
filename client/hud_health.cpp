Aynekko/Diffusion

commit fab8d384ad04221daa60e723cce599ae90dc2cf9

Experimental: cache Mod_BoxVisible result during normalpass, use r_cached_box_culling to disable it

client/render/r_cvars.h
@@ -68,6 +68,7 @@ extern cvar_t	*r_fade_props;
 extern cvar_t	*r_show_cubemaps;
 extern cvar_t	*r_bloom_sprites;
 extern cvar_t	*r_bloom;
+extern cvar_t *r_cached_box_culling;
 extern cvar_t *r_blur;
 extern cvar_t *r_blur_threshold;
 extern cvar_t *r_blur_strength;

client/render/r_local.h
@@ -307,6 +307,7 @@ typedef struct
 
 	float		viewplanedist;
 	mplane_t		clipPlane;
+	int bBoxVisible[8192]; // cache cull state for an entity here if the leaf is unchanged !!! NORMALPASS ONLY (I'm scared to break stuff in other passes yet)
 } ref_instance_t;
 
 typedef struct

client/render/r_main.cpp
@@ -1046,6 +1046,14 @@ void R_FindViewLeaf( void )
 {
 	RI->oldviewleaf = RI->viewleaf;
 	RI->viewleaf = Mod_PointInLeaf( RI->pvsorigin, worldmodel->nodes );
+	if( RP_NORMALPASS() ) // yes, normalpass only. just in case - don't mess with other passes...
+	{
+		if( RI->viewleaf != RI->oldviewleaf )
+		{
+			memset( RI->bBoxVisible, -1, sizeof( RI->bBoxVisible ) ); // reset entity cached cull state
+		//	Msg( "cull cache reset!\n" );
+		}
+	}
 }
 
 /*

client/render/r_misc.cpp
@@ -2021,6 +2021,7 @@ void R_NewMap( void )
 
 	tr.realframecount = 0;
 	RI->viewleaf = NULL; // it's may be data from previous map
+	memset( RI->bBoxVisible, -1, sizeof( RI->bBoxVisible ) ); // reset entity cached cull state
 
 	// setup the skybox sides
 	for( i = 0; i < 6; i++ )

client/render/r_world.cpp
@@ -3416,6 +3416,7 @@ void R_MarkLeaves( void )
 		ClearBits( r_novis->flags, FCVAR_CHANGED );
 		tr.fResetVis = false;
 		RI->viewleaf = NULL;
+		memset( RI->bBoxVisible, -1, sizeof( RI->bBoxVisible ) ); // reset entity cached cull state
 	}
 
 	if( RI->viewleaf == RI->oldviewleaf && RI->viewleaf != NULL )

client/render/view.cpp
@@ -125,6 +125,7 @@ cvar_t *r_fade_props;
 cvar_t *r_show_cubemaps;
 cvar_t *r_bloom_sprites;
 cvar_t *r_bloom;
+cvar_t *r_cached_box_culling;
 cvar_t *thirdperson;
 cvar_t *r_sun_allowed;
 cvar_t *cl_hitsound;
@@ -384,6 +385,7 @@ void V_Init( void )
 	r_fade_props = CVAR_REGISTER( "r_fade_props", "1", FCVAR_ARCHIVE );
 	r_show_cubemaps = CVAR_REGISTER( "r_show_cubemaps", "0", FCVAR_ARCHIVE );
 	r_bloom_sprites = CVAR_REGISTER( "r_bloom_sprites", "0", FCVAR_ARCHIVE );
+	r_cached_box_culling = CVAR_REGISTER( "r_cached_box_culling", "1", FCVAR_ARCHIVE );
 	r_bloom = CVAR_REGISTER( "r_bloom", "0", FCVAR_ARCHIVE );
 	thirdperson = CVAR_REGISTER( "thirdperson", "0", FCVAR_CHEAT );
 	r_blur = CVAR_REGISTER( "r_blur", "1", FCVAR_ARCHIVE );

client/utils.cpp
@@ -286,6 +286,15 @@ bool Mod_BoxVisible( const Vector mins, const Vector maxs, const byte *visbits )
 	if( !visbits || !mins || !maxs )
 		return true;
 
+	// during normalpass, use cached cull value, if the viewleaf is the same as last frame.
+	// this culling is expensive stuff
+	bool use_cache = (r_cached_box_culling->value > 0 && RI->currententity != NULL && (RI->currententity->index > 0) && (RI->currententity->curstate.origin == RI->currententity->prevstate.origin) && RP_NORMALPASS());
+	if( use_cache )
+	{
+		if( RI->bBoxVisible[RI->currententity->index] != -1 )
+			return RI->bBoxVisible[RI->currententity->index] > 0 ? true : false;
+	}
+
 	count = Mod_BoxLeafnums( mins, maxs, leafList, SIZEOFARRAY( leafList ), &headnode );
 
 	if( count < SIZEOFARRAY( leafList ))
@@ -293,12 +302,23 @@ bool Mod_BoxVisible( const Vector mins, const Vector maxs, const byte *visbits )
 
 	for( i = 0; i < count; i++ )
 	{
-		if( CHECKVISBIT( visbits, leafList[i] ))
+		if( CHECKVISBIT( visbits, leafList[i] ) )
+		{
+			if( use_cache )
+				RI->bBoxVisible[RI->currententity->index] = 1;
 			return true;
+		}
 	}
 
-	if( Mod_HeadnodeVisible( headnode, visbits ))
+	if( Mod_HeadnodeVisible( headnode, visbits ) )
+	{
+		if( use_cache )
+			RI->bBoxVisible[RI->currententity->index] = 1;
 		return true;
+	}
+
+	if( use_cache )
+		RI->bBoxVisible[RI->currententity->index] = 0;
 
 	return false;
 }

common/const.h
@@ -155,11 +155,12 @@
 #define EF_SKIPPVS			BIT(20)	// this entity skips PVS check in AddToFullPack, thus always present at all times
 #define EF_ROTATING			BIT(21) // client-side func_rotating - non-solid and very simple
 #define EF_UPSIDEDOWN		BIT(22) // player is upside down
+// BIT(23) // unused
 #define EF_NOREFLECT		BIT(24)	// Entity won't reflecting in mirrors
 #define EF_REFLECTONLY		BIT(25)	// Entity will be drawing only in mirrors
 #define EF_WATERSIDES		BIT(26)	// Do not remove sides for func_water entity
-//#define ...			BIT(27) // unused
-//#define ...			BIT(28) // unused
+#define EF_FULLBRIGHT		BIT(27) // used in engine
+#define EF_NOSHADOW			BIT(28) // used in engine
 #define EF_MERGE_VISIBILITY	BIT(29)	// this entity allowed to merge vis (e.g. env_sky or portal camera)
 #define EF_REQUEST_PHS		BIT(30)	// This entity requested phs bitvector instead of pvsbitvector in AddToFullPack calls
 
@@ -173,7 +174,7 @@
 #define CF_MOVIE_SOUND		(1<<2)	// allow sound
 
 // custom flags for env_projector
-//#define CF_NOATTEN			BIT(0)	// don't use attenuation for this light // diffusion - NOW UNUSED
+// BIT(0)	// unused
 #define CF_TEXTURE			BIT(1)	// custom texture specified
 #define CF_SPRITE			BIT(2)	// custom sprite specified
 #define CF_MOVIE			BIT(3)	// custom movie specified

commit 7fbb62f44e34136753563d65f77d9407e1233c78

Maintenance - fix hud/ui flickering due to unsufficient clean-up in SSAO - fix FillRoundedRGBA (or rather not fix, just using different code...lol!) - hud_health: too many sin calculations, make it one - REMOVE cached box culling (there are weird mistakes which I don't want to bother fixing) (introduced in fab8d384ad04221daa60e723cce599ae90dc2cf9 )

client/hud_health.cpp
@@ -107,12 +107,13 @@ int CHudHealthVisual :: Draw(float flTime)
 	float cell_start_y = pos_y;
 	int cell;
 
+	float transp = (health_val < 25.f) ? (Transparency + fabs( sin( tr.time * 3 ) ) * 0.25f) : Transparency;
 	for( cell = 0; cell < total_cells; cell++ )
 	{
 		if( cell >= health_val * 0.5f ) // draw grey cells
 			FillRoundedRGBA( cell_start_x, cell_start_y, cell_width, cell_height, 3, Vector4D( 0.5f, 0.5f, 0.5f, 0.5f ) );
 		else
-			FillRoundedRGBA( cell_start_x, cell_start_y, cell_width, cell_height, 3, Vector4D( r, g, b, (health_val < 25.f) ? (Transparency + fabs( sin( tr.time * 3 ) ) * 0.25f) : Transparency ) );
+			FillRoundedRGBA( cell_start_x, cell_start_y, cell_width, cell_height, 3, Vector4D( r, g, b, transp ) );
 		cell_start_x += cell_width + cell_margin;
 	}
 

client/render/r_backend.cpp
@@ -439,113 +439,63 @@ bool GL_UsingAlphaToCoverage( void )
 	return (CVAR_GET_FLOAT( "gl_msaa" ) > 0.0f);
 }
 
-//=====================================================================================
+//=============================================================================================================
 // FillRoundedRGBA
-// credits: https://stackoverflow.com/users/2521214/spektre
-//=====================================================================================
-void FillRoundedRGBA( float posx, float posy, float w, float h, float r, Vector4D rgba )
+// credits: https://stackoverflow.com/questions/5369507/opengl-es-1-0-2d-rounded-rectangle by user6458202
+//=============================================================================================================
+#define GLW_SMALL_ROUNDED_CORNER_SLICES 5  // How many vertexes you want of each corner
+static Vector2D glwRoundedCorners[GLW_SMALL_ROUNDED_CORNER_SLICES] = { Vector2D( 0, 0 ) }; // This array keep the generated vertexes of one corner
+static void createRoundedCorners( Vector2D *arr, int num )
 {
-	// so it would have the same behavior as FillRGBA
-	posx += w * 0.5f;
-	posy += h * 0.5f;
+	// Generate the corner vertexes
+	float slice = M_PI / 2 / num;
+	float a = 0;
+	for( int i = 0; i < num; a += slice, ++i )
+	{
+		arr[i].x = cosf( a );
+		arr[i].y = sinf( a );
+	}
+}
+void FillRoundedRGBA( float x, float y, float width, float height, float radius, Vector4D rgba )
+{
+	if( glwRoundedCorners[0].x + glwRoundedCorners[0].y == 0 )
+		createRoundedCorners( glwRoundedCorners, GLW_SMALL_ROUNDED_CORNER_SLICES );
+
 	rgba.x = bound( 0.0f, rgba.x, 1.0f );
 	rgba.y = bound( 0.0f, rgba.y, 1.0f );
 	rgba.z = bound( 0.0f, rgba.z, 1.0f );
 	rgba.w = bound( 0.0f, rgba.w, 1.0f );
-	if( r > w * 0.5f ) r = w * 0.5f;
-	if( r > h * 0.5f ) r = h * 0.5f;
 
+	float left = x;
+	float top = y;
+	float bottom = y + height - 1;
+	float right = x + width - 1;
 	int i;
-	float x0, y0, x, y;
-	const float sina[45] = { 
-		0,
-		0.1736482,
-		0.3420201,
-		0.5,
-		0.6427876,
-		0.7660444,
-		0.8660254,
-		0.9396926,
-		0.9848077,
-		1,
-		0.9848078,
-		0.9396927,
-		0.8660255,
-		0.7660446,
-		0.6427878,
-		0.5000002,
-		0.3420205,
-		0.1736485,
-		3.894144E-07,
-		-0.1736478,
-		-0.3420197,
-		-0.4999996,
-		-0.6427872,
-		-0.7660443,
-		-0.8660252,
-		-0.9396925,
-		-0.9848077,
-		-1,
-		-0.9848078,
-		-0.9396928,
-		-0.8660257,
-		-0.7660449,
-		-0.6427881,
-		-0.5000006,
-		-0.3420208,
-		-0.1736489,
-		0,
-		0.1736482,
-		0.3420201,
-		0.5,
-		0.6427876,
-		0.7660444,
-		0.8660254,
-		0.9396926,
-		0.9848077 };
-	const float *cosa = sina + 9;
-	w -= r + r;
-	h -= r + r;
-
+	if( radius > width * 0.5f ) radius = width * 0.5f;
+	if( radius > height * 0.5f ) radius = height * 0.5f;
+	pglDisable( GL_TEXTURE_2D );
 	GL_Texture2D( GL_FALSE );
 	GL_Blend( GL_TRUE );
 	GL_BlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
 	GL_Color4f( rgba.x, rgba.y, rgba.z, rgba.w );
-
-	pglBegin( GL_TRIANGLE_FAN );
-	pglVertex2f( posx, posy );
-	x0 = posx + (0.5 * w);
-	y0 = posy + (0.5 * h);
-	for( i = 0; i < 9; i++ )
+	pglBegin( GL_QUAD_STRIP );
+	// Draw left rounded side.
+	for( i = 0; i < GLW_SMALL_ROUNDED_CORNER_SLICES; ++i )
 	{
-		x = x0 + (r * cosa[i]);
-		y = y0 + (r * sina[i]);
-		pglVertex2f( x, y );
+		pglVertex2f( left + radius - radius * glwRoundedCorners[i].x,
+			bottom - radius + radius * glwRoundedCorners[i].y );
+		pglVertex2f( left + radius - radius * glwRoundedCorners[i].x,
+			top + radius - radius * glwRoundedCorners[i].y );
 	}
-	x0 -= w;
-	for( ; i < 18; i++ )
+	// Draw right rounded side.
+	for( i = GLW_SMALL_ROUNDED_CORNER_SLICES - 1; i >= 0; --i )
 	{
-		x = x0 + (r * cosa[i]);
-		y = y0 + (r * sina[i]);
-		pglVertex2f( x, y );
+		pglVertex2f( right - radius + radius * glwRoundedCorners[i].x,
+			bottom - radius + radius * glwRoundedCorners[i].y );
+		pglVertex2f( right - radius + radius * glwRoundedCorners[i].x,
+			top + radius - radius * glwRoundedCorners[i].y );
 	}
-	y0 -= h;
-	for( ; i < 27; i++ )
-	{
-		x = x0 + (r * cosa[i]);
-		y = y0 + (r * sina[i]);
-		pglVertex2f( x, y );
-	}
-	x0 += w;
-	for( ; i < 36; i++ )
-	{
-		x = x0 + (r * cosa[i]);
-		y = y0 + (r * sina[i]);
-		pglVertex2f( x, y );
-	}
-	pglVertex2f( x, posy + (0.5 * h) );
 	pglEnd();
-
 	GL_Texture2D( GL_TRUE );
 	GL_Blend( GL_FALSE );
 }

client/render/r_cvars.h
@@ -67,7 +67,6 @@ extern cvar_t	*r_fade_props;
 extern cvar_t	*r_show_cubemaps;
 extern cvar_t	*r_bloom_sprites;
 extern cvar_t	*r_bloom;
-extern cvar_t *r_cached_box_culling;
 extern cvar_t *r_blur;
 extern cvar_t *r_blur_threshold;
 extern cvar_t *r_blur_strength;

client/render/r_local.h
@@ -307,7 +307,6 @@ typedef struct
 
 	float		viewplanedist;
 	mplane_t		clipPlane;
-	int bBoxVisible[8192]; // cache cull state for an entity here if the leaf is unchanged !!! NORMALPASS ONLY (I'm scared to break stuff in other passes yet)
 } ref_instance_t;
 
 typedef struct

client/render/r_main.cpp
@@ -1055,14 +1055,6 @@ void R_FindViewLeaf( void )
 {
 	RI->oldviewleaf = RI->viewleaf;
 	RI->viewleaf = Mod_PointInLeaf( RI->pvsorigin, worldmodel->nodes );
-	if( RP_NORMALPASS() ) // yes, normalpass only. just in case - don't mess with other passes...
-	{
-		if( RI->viewleaf != RI->oldviewleaf )
-		{
-			memset( RI->bBoxVisible, -1, sizeof( RI->bBoxVisible ) ); // reset entity cached cull state
-		//	Msg( "cull cache reset!\n" );
-		}
-	}
 }
 
 /*

client/render/r_misc.cpp
@@ -2021,7 +2021,6 @@ void R_NewMap( void )
 
 	tr.realframecount = 0;
 	RI->viewleaf = NULL; // it's may be data from previous map
-	memset( RI->bBoxVisible, -1, sizeof( RI->bBoxVisible ) ); // reset entity cached cull state
 
 	// setup the skybox sides
 	for( i = 0; i < 6; i++ )

client/render/r_postprocess.cpp
@@ -857,7 +857,7 @@ void SSAO( void )
 
 	// unbind shader
 	GL_BindShader( NULL );
-	GL_CleanUpTextureUnits( 0 );
+	GL_CleanupAllTextureUnits();
 
 	r_stats.sh_ssao_pass++;
 }

client/render/r_world.cpp
@@ -3412,7 +3412,6 @@ void R_MarkLeaves( void )
 		ClearBits( r_novis->flags, FCVAR_CHANGED );
 		tr.fResetVis = false;
 		RI->viewleaf = NULL;
-		memset( RI->bBoxVisible, -1, sizeof( RI->bBoxVisible ) ); // reset entity cached cull state
 	}
 
 	if( RI->viewleaf == RI->oldviewleaf && RI->viewleaf != NULL )

client/render/view.cpp
@@ -124,7 +124,6 @@ cvar_t *r_fade_props;
 cvar_t *r_show_cubemaps;
 cvar_t *r_bloom_sprites;
 cvar_t *r_bloom;
-cvar_t *r_cached_box_culling;
 cvar_t *thirdperson;
 cvar_t *r_sun_allowed;
 cvar_t *cl_hitsound;
@@ -383,7 +382,6 @@ void V_Init( void )
 	r_fade_props = CVAR_REGISTER( "r_fade_props", "1", FCVAR_ARCHIVE );
 	r_show_cubemaps = CVAR_REGISTER( "r_show_cubemaps", "0", FCVAR_ARCHIVE );
 	r_bloom_sprites = CVAR_REGISTER( "r_bloom_sprites", "0", FCVAR_ARCHIVE );
-	r_cached_box_culling = CVAR_REGISTER( "r_cached_box_culling", "1", FCVAR_ARCHIVE );
 	r_bloom = CVAR_REGISTER( "r_bloom", "0", FCVAR_ARCHIVE );
 	thirdperson = CVAR_REGISTER( "thirdperson", "0", FCVAR_CHEAT );
 	r_blur = CVAR_REGISTER( "r_blur", "1", FCVAR_ARCHIVE );

client/utils.cpp
@@ -279,46 +279,26 @@ is potentially visible
 */
 bool Mod_BoxVisible( const Vector mins, const Vector maxs, const byte *visbits )
 {
-	short	leafList[2048];
-	mnode_t	*headnode;
+	short leafList[2048];
+	mnode_t *headnode;
 	int	i, count;
 
 	if( !visbits || !mins || !maxs )
 		return true;
 
-	// during normalpass, use cached cull value, if the viewleaf is the same as last frame.
-	// this culling is expensive stuff
-	bool use_cache = (r_cached_box_culling->value > 0 && RI->currententity != NULL && (RI->currententity->index > 0) && (RI->currententity->curstate.origin == RI->currententity->prevstate.origin) && RP_NORMALPASS());
-	if( use_cache )
-	{
-		if( RI->bBoxVisible[RI->currententity->index] != -1 )
-			return RI->bBoxVisible[RI->currententity->index] > 0 ? true : false;
-	}
-
 	count = Mod_BoxLeafnums( mins, maxs, leafList, SIZEOFARRAY( leafList ), &headnode );
 
-	if( count < SIZEOFARRAY( leafList ))
+	if( count < SIZEOFARRAY( leafList ) )
 		headnode = NULL; // ignore headnode if we not overflowed
 
 	for( i = 0; i < count; i++ )
 	{
 		if( CHECKVISBIT( visbits, leafList[i] ) )
-		{
-			if( use_cache )
-				RI->bBoxVisible[RI->currententity->index] = 1;
 			return true;
-		}
 	}
 
 	if( Mod_HeadnodeVisible( headnode, visbits ) )
-	{
-		if( use_cache )
-			RI->bBoxVisible[RI->currententity->index] = 1;
 		return true;
-	}
-
-	if( use_cache )
-		RI->bBoxVisible[RI->currententity->index] = 0;
 
 	return false;
 }