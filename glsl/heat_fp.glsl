513d817092b2837313d48d53ac550ddc2d9bfab0
Aynekko <5f@mail.ru>
2024-12-18 23:54:43 +0300

    Heat shader: small optimize

--- glsl/heat_fp.glsl
+++ glsl/heat_fp.glsl
@@ -32,25 +32,23 @@ void main( void )
 	float fSampledDepth = texture2D( u_DepthMap, var_TexCoord ).r;
 	fSampledDepth = linearizeDepth( 4096, fSampledDepth );
 	fSampledDepth = RemapVal( fSampledDepth, Z_NEAR, 4096, 0.0, 1.0 );
+
+	if( fSampledDepth <= 0.5 )
+		discard;
 	
 	vec2 p_m = gl_FragCoord.xy / u_ScreenSize;
 	vec2 dst_offset = vec2(0.0);
 
-	if( fSampledDepth > 0.5 )
-	{
-		vec2 p_d = p_m;
+	vec2 p_d = p_m;
 
-		p_d.y -= u_RealTime * 0.025 * ( 1.0 / fSampledDepth );
-    
-		vec4 dst_map_val = vec4( noise(p_d * vec2(50)) * 0.25 * u_Scale );
-    
-		dst_offset = dst_map_val.xy;
-		dst_offset *= 2.;
-		dst_offset *= 0.01;
-	
-		// reduce effect towards Y top
-		dst_offset *= (1. - p_m.y);
-	}
+	p_d.y -= u_RealTime * 0.025 * ( 1.0 / fSampledDepth );
+
+	vec4 dst_map_val = vec4( noise(p_d * vec2(50)) * 0.25 * u_Scale );
+
+	dst_offset = dst_map_val.xy * 0.02;
+
+	// reduce effect towards Y top
+	dst_offset *= (1.0 - p_m.y);
     
 	vec2 dist_tex_coord = p_m.xy + dst_offset;
 	vec4 original = texture2D(u_ColorMap, var_TexCoord);
