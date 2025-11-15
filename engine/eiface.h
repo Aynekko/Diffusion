76a2f3cb48240b6214122ea52a2caab20fbdfe63
Alibek Omarov <a1ba.omarov@gmail.com>
2025-11-15 22:02:22 +0500

    engine: eiface: use compiler's offsetof, if possible

--- engine/eiface.h
+++ engine/eiface.h
@@ -38,7 +38,11 @@
 #include "exportdef.h"
 
 #ifndef offsetof
+#ifdef __GNUC__
+#define offsetof(s,m)	__builtin_offsetof(s,m)
+#else
 #define offsetof(s,m)	(size_t)&(((s *)0)->m)
+#endif
 #endif
 
 #include <alert.h>
