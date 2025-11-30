Aynekko/Diffusion

commit b16d0b42a6504c32e11f512aadc9b98885f17381

Reworked dynamic ammo in crates, added unauth. weapon drop, decreases some ammo capacities, increased shotgun reloading speed

server/weapons/weapons.cpp
@@ -537,6 +537,7 @@ BEGIN_DATADESC( CBasePlayerItem )
 	DEFINE_FUNCTION( Materialize ),
 	DEFINE_FUNCTION( AttemptToMaterialize ),
 	DEFINE_FUNCTION( Kill ),
+	DEFINE_FUNCTION( UnauthorizedUse ),
 END_DATADESC()
 
 BEGIN_DATADESC( CBasePlayerWeapon )
@@ -567,13 +568,38 @@ void CBasePlayerItem :: FallInit( void )
 
 	UTIL_SetSize(pev, g_vecZero, g_vecZero );//pointsize until it lands on the ground.
 	
-	SetTouch( &CBasePlayerItem::DefaultTouch );
-	SetThink(&CBasePlayerItem::FallThink );
+	if( HasFlag( F_WEAPON_LOCKED ) )
+	{
+		SetUse( &CBasePlayerItem::UnauthorizedUse );
+		ObjectCaps();
+		pev->solid = SOLID_TRIGGER;
+		UTIL_SetSize( pev, Vector( -4, -4, 0 ), Vector( 4, 4, 4 ) );
+	}
+	else
+		SetTouch( &CBasePlayerItem::DefaultTouch );
+
+	SetThink( &CBasePlayerItem::FallThink );
 
 	if( !pev->iuser4 || pev->iuser4 <= 0 )
 		SetFadeDistance( 1000 ); // diffusion - UNDONE should be a cvar?
 
 	SetNextThink( 0 );
+}
+
+void CBasePlayerItem::UnauthorizedUse( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value )
+{
+	if( !pActivator || !pActivator->IsPlayer() || !pActivator->IsAlive() )
+		return;
+
+	pActivator->TakeDamage( pev, pev, 25.0f, DMG_SHOCK );
+	UTIL_ShowMessage( "UTIL_UNAUTH", pActivator );
+	UTIL_DoSpark( pev, GetAbsOrigin() );
+	// add glitch effect
+	MESSAGE_BEGIN( MSG_ONE, gmsgTempEnt, NULL, pActivator->pev );
+		WRITE_BYTE( TE_PLAYER_GLITCH );
+		WRITE_BYTE( 5 ); // x0.1
+		WRITE_BYTE( 1 ); // hold 1 second
+	MESSAGE_END();
 }
 
 //=========================================================
@@ -637,6 +663,14 @@ void CBasePlayerItem::FallThink ( void )
 	}
 }
 
+int CBasePlayerItem::ObjectCaps( void )
+{
+	if( HasFlag( F_WEAPON_LOCKED ) )
+		return FCAP_IMPULSE_USE;
+
+	return 0;
+}
+
 //=========================================================
 // Materialize - make a CBasePlayerItem visible and tangible
 //=========================================================
@@ -650,10 +684,13 @@ void CBasePlayerItem::Materialize( void )
 		pev->effects |= EF_MUZZLEFLASH;
 	}
 
-	pev->solid = SOLID_TRIGGER;
+	if( !HasFlag( F_WEAPON_LOCKED ) )
+	{
+		pev->solid = SOLID_TRIGGER;
+		SetTouch( &CBasePlayerItem::DefaultTouch );
+	}
 
 	RelinkEntity( TRUE ); // link into world.
-	SetTouch (&CBasePlayerItem::DefaultTouch);
 	SetThink (NULL);
 
 	if( HasFlag( F_WEAPON_DESPAWN ) )

common/const.h
@@ -80,6 +80,7 @@
 #define F_PLAYER_DRONE			BIT(21) // it's a _playerdrone
 #define F_MONSTER_CAN_SEE_THROUGH BIT(22) // monsters can now see through transparent brushes, this is a workaround for mapper's purposes
 #define F_PLAYER_CONTROL BIT(23) // this entity is controlled by player (i.e. drone)
+#define F_WEAPON_LOCKED BIT(24) // unathorized weapon which zaps the grabber on use
 
 // Goes into globalvars_t.trace_flags
 #define FTRACE_SIMPLEBOX		(1<<0)	// Traceline with a simple box

common/weaponinfo.h
@@ -150,7 +150,9 @@ const char * const g_WpnAmmo[] =
 // weapon clip/carry ammo capacities
 #define URANIUM_MAX_CARRY		10
 #define AR2_MAX_CARRY			200
-#define	_9MM_MAX_CARRY			200
+#define G36C_MAX_CARRY			120
+#define MRC_MAX_CARRY			150
+#define	_9MM_MAX_CARRY			90
 #define DEAGLE_MAX_CARRY		21
 #define BUCKSHOT_MAX_CARRY		40
 #define BOLT_MAX_CARRY			15
@@ -597,14 +599,14 @@ enum beretta_e
 #define SHOTGUN_NEXT_PA_TIME	0.9
 #define SHOTGUN_NEXT_SA_TIME	1.2
 #define SHOTGUN_STARTRELOAD_TIME 0.6
-#define SHOTGUN_RELOAD_TIME		0.7
+#define SHOTGUN_RELOAD_TIME		0.6
 #define SHOTGUN_RELOAD_FINISH_TIME 0.7
 #define SHOTGUN_RELOADEMPTY_FINISH_TIME 1.0
 
 #define SHOTGUNXM_DEPLOY_TIME	0.6
 #define SHOTGUNXM_NEXT_PA_TIME	0.5
 #define SHOTGUNXM_STARTRELOAD_TIME 0.4
-#define SHOTGUNXM_RELOAD_TIME	0.6
+#define SHOTGUNXM_RELOAD_TIME	0.55
 #define SHOTGUNXM_RELOAD_FINISH_TIME 0.8
 #define SHOTGUNXM_RELOADEMPTY_FINISH_TIME 1.5
 #define SHOTGUNXM_DRAWRELOADEMPTY_FINISH_TIME 1.8

server/bot/bot_combat.cpp
@@ -124,9 +124,9 @@ static const float secondary_fire_delay[WEAPON_G36C +1][5][2] = {
 ammo_check_t ammo_check[] = {
    {"ammo_glockclip", g_WpnAmmo[WEAPON_BERETTA], _9MM_MAX_CARRY},
    {"ammo_9mmclip", g_WpnAmmo[WEAPON_BERETTA], _9MM_MAX_CARRY},
-   {"ammo_9mmAR", g_WpnAmmo[WEAPON_MRC], _9MM_MAX_CARRY},
+   {"ammo_9mmAR", g_WpnAmmo[WEAPON_MRC], MRC_MAX_CARRY},
    {"ammo_9mmbox", g_WpnAmmo[WEAPON_BERETTA], _9MM_MAX_CARRY},
-   {"ammo_mp5clip", g_WpnAmmo[WEAPON_MRC], _9MM_MAX_CARRY},
+   {"ammo_mp5clip", g_WpnAmmo[WEAPON_MRC], MRC_MAX_CARRY},
    {"ammo_chainboxclip", g_WpnAmmo[WEAPON_BERETTA], _9MM_MAX_CARRY},
    {"ammo_mp5grenades", "ARgrenades", M203_GRENADE_MAX_CARRY},
    {"ammo_ARgrenades", "ARgrenades", M203_GRENADE_MAX_CARRY},
@@ -140,7 +140,7 @@ ammo_check_t ammo_check[] = {
    {"ammo_ar2", g_WpnAmmo[WEAPON_AR2], AR2_MAX_CARRY},
    {"ammo_ar2ball", "ar2ball", M203_GRENADE_MAX_CARRY},
    {"ammo_fiveseven", g_WpnAmmo[WEAPON_FIVESEVEN], _57_MAX_CARRY },
-   {"ammo_g36c", g_WpnAmmo[WEAPON_G36C], _9MM_MAX_CARRY},
+   {"ammo_g36c", g_WpnAmmo[WEAPON_G36C], G36C_MAX_CARRY},
    {"ammo_hkmp5", g_WpnAmmo[WEAPON_HKMP5], _9MM_MAX_CARRY },
 	{"ammo_sniper", g_WpnAmmo[WEAPON_SNIPER], SNIPER_MAX_CARRY},
    {"", 0, 0}};

server/entities/ammocrate.cpp
@@ -318,7 +318,7 @@ void CAmmoCrate::GiveItems( CBasePlayer *pPlayer )
 				Ratio[WEAPON_DEAGLE] = (float)pPlayer->AmmoInventory( pPlayer->GetAmmoIndex( g_WpnAmmo[WEAPON_DEAGLE] ) ) / DEAGLE_MAX_CARRY;
 				break;
 			case WEAPON_MRC:
-				Ratio[WEAPON_MRC] = (float)pPlayer->AmmoInventory( pPlayer->GetAmmoIndex( g_WpnAmmo[WEAPON_MRC] ) ) / _9MM_MAX_CARRY;
+				Ratio[WEAPON_MRC] = (float)pPlayer->AmmoInventory( pPlayer->GetAmmoIndex( g_WpnAmmo[WEAPON_MRC] ) ) / MRC_MAX_CARRY;
 				break;
 			case WEAPON_CROSSBOW:
 				Ratio[WEAPON_CROSSBOW] = (float)pPlayer->AmmoInventory( pPlayer->GetAmmoIndex( g_WpnAmmo[WEAPON_CROSSBOW] ) ) / BOLT_MAX_CARRY;
@@ -353,7 +353,7 @@ void CAmmoCrate::GiveItems( CBasePlayer *pPlayer )
 				Ratio[WEAPON_SNIPER] = (float)pPlayer->AmmoInventory( pPlayer->GetAmmoIndex( g_WpnAmmo[WEAPON_SNIPER] ) ) / SNIPER_MAX_CARRY;
 				break;
 			case WEAPON_G36C:
-				Ratio[WEAPON_G36C] = (float)pPlayer->AmmoInventory( pPlayer->GetAmmoIndex( g_WpnAmmo[WEAPON_G36C] ) ) / _9MM_MAX_CARRY;
+				Ratio[WEAPON_G36C] = (float)pPlayer->AmmoInventory( pPlayer->GetAmmoIndex( g_WpnAmmo[WEAPON_G36C] ) ) / G36C_MAX_CARRY;
 				break;
 		//	case WEAPON_RPG:
 		//		Ratio[WEAPON_RPG] = (float)pPlayer->AmmoInventory( pPlayer->GetAmmoIndex( g_WpnAmmo[WEAPON_RPG] ) ) / ROCKET_MAX_CARRY;
@@ -411,7 +411,15 @@ void CAmmoCrate::GiveItems( CBasePlayer *pPlayer )
 		}
 	}
 
-	GiveDynamicAmmo( pPlayer, LowestAmmoId2, RatioAmmoId2 * 0.8f );
+	// add ratio of the first given ammo, to the next ratio, so it won't give too much
+	float newratio = (RatioAmmoId1 * 3.0f) + (RatioAmmoId2 * 0.8f);
+	if( newratio >= 0.99f )
+	{
+		PlayPickupSound( pPlayer );
+		return;
+	}
+
+	GiveDynamicAmmo( pPlayer, LowestAmmoId2, newratio );
 	if( !HasSpawnFlags( SF_DYNAMIC_RESUPPLY ) )
 	{
 		m_iCounter--;
@@ -422,7 +430,14 @@ void CAmmoCrate::GiveItems( CBasePlayer *pPlayer )
 		}
 	}
 
-	GiveDynamicAmmo( pPlayer, LowestAmmoId3, RatioAmmoId3 * 0.65f );
+	newratio = (newratio * 3.0f) + (RatioAmmoId3 * 0.65f);
+	if( newratio >= 0.99f )
+	{
+		PlayPickupSound( pPlayer );
+		return;
+	}
+
+	GiveDynamicAmmo( pPlayer, LowestAmmoId3, newratio );
 	PlayPickupSound( pPlayer );
 }
 
@@ -462,9 +477,9 @@ void CAmmoCrate::GiveDynamicAmmo( CBasePlayer *pPlayer, int WeaponID, float Curr
 		DbgPrint( pPlayer->AmmoInventory( pPlayer->GetAmmoIndex( g_WpnAmmo[WEAPON_DEAGLE] ) ), g_WpnAmmo[WEAPON_DEAGLE], DEAGLE_MAX_CARRY, iGive );
 		break;
 	case WEAPON_MRC:
-		iGive = CalcGive( _9MM_MAX_CARRY, CurrentRatio );
-		pPlayer->GiveAmmo( iGive, g_WpnAmmo[WEAPON_MRC], _9MM_MAX_CARRY );
-		DbgPrint( pPlayer->AmmoInventory( pPlayer->GetAmmoIndex( g_WpnAmmo[WEAPON_MRC] ) ), g_WpnAmmo[WEAPON_MRC], _9MM_MAX_CARRY, iGive );
+		iGive = CalcGive( MRC_MAX_CARRY, CurrentRatio );
+		pPlayer->GiveAmmo( iGive, g_WpnAmmo[WEAPON_MRC], MRC_MAX_CARRY );
+		DbgPrint( pPlayer->AmmoInventory( pPlayer->GetAmmoIndex( g_WpnAmmo[WEAPON_MRC] ) ), g_WpnAmmo[WEAPON_MRC], MRC_MAX_CARRY, iGive );
 		break;
 	case WEAPON_CROSSBOW:
 		iGive = CalcGive( BOLT_MAX_CARRY, CurrentRatio );
@@ -507,9 +522,9 @@ void CAmmoCrate::GiveDynamicAmmo( CBasePlayer *pPlayer, int WeaponID, float Curr
 		DbgPrint( pPlayer->AmmoInventory( pPlayer->GetAmmoIndex( g_WpnAmmo[WEAPON_SHOTGUN_XM] ) ), g_WpnAmmo[WEAPON_SHOTGUN_XM], BUCKSHOT_MAX_CARRY, iGive );
 		break;
 	case WEAPON_G36C:
-		iGive = CalcGive( _9MM_MAX_CARRY, CurrentRatio );
-		pPlayer->GiveAmmo( iGive, g_WpnAmmo[WEAPON_G36C], _9MM_MAX_CARRY );
-		DbgPrint( pPlayer->AmmoInventory( pPlayer->GetAmmoIndex( g_WpnAmmo[WEAPON_G36C] ) ), g_WpnAmmo[WEAPON_G36C], _9MM_MAX_CARRY, iGive );
+		iGive = CalcGive( G36C_MAX_CARRY, CurrentRatio );
+		pPlayer->GiveAmmo( iGive, g_WpnAmmo[WEAPON_G36C], G36C_MAX_CARRY );
+		DbgPrint( pPlayer->AmmoInventory( pPlayer->GetAmmoIndex( g_WpnAmmo[WEAPON_G36C] ) ), g_WpnAmmo[WEAPON_G36C], G36C_MAX_CARRY, iGive );
 		break;
 	case WEAPON_RPG:
 		iGive = CalcGive( ROCKET_MAX_CARRY, CurrentRatio );

server/game/rules_multiplayer.cpp
@@ -954,12 +954,12 @@ void CHalfLifeMultiplay :: PlayerSpawn( CBasePlayer *pPlayer )
 		break;
 		case 4:
 			pPlayer->GiveNamedItem("weapon_9mmAR");
-			pPlayer->GiveAmmo(MRC_DEFAULT_GIVE, g_WpnAmmo[WEAPON_MRC], _9MM_MAX_CARRY);
+			pPlayer->GiveAmmo(MRC_DEFAULT_GIVE, g_WpnAmmo[WEAPON_MRC], MRC_MAX_CARRY );
 			pPlayer->m_iBonusWeaponID = WEAPON_MRC;
 		break;
 		case 5:
 			pPlayer->GiveNamedItem( "weapon_g36c" );
-			pPlayer->GiveAmmo( G36C_DEFAULT_GIVE, g_WpnAmmo[WEAPON_G36C], _9MM_MAX_CARRY );
+			pPlayer->GiveAmmo( G36C_DEFAULT_GIVE, g_WpnAmmo[WEAPON_G36C], G36C_MAX_CARRY );
 			pPlayer->m_iBonusWeaponID = WEAPON_G36C;
 		break;
 		case 6:

server/monsters/entities/monster_human_grunt.cpp
@@ -1627,10 +1627,20 @@ void CHGrunt :: HandleAnimEvent( MonsterEvent_t *pEvent )
 					SetBodygroup( GUN_GROUP, GUN_NONE );
 
 					// now spawn a gun.
-					if (HasWeapon( HGRUNT_SHOTGUN ))
-						 DropItem( "weapon_shotgun", vecGunPos, vecGunAngles );
+					if( !HasSpawnFlags( SF_MONSTER_WEAPON_LOCKED ) )
+					{
+						if( HasWeapon( HGRUNT_SHOTGUN ) )
+							DropItem( "weapon_shotgun", vecGunPos, vecGunAngles );
+						else
+							DropItem( "weapon_mrc", vecGunPos, vecGunAngles );
+					}
 					else
-						 DropItem( "weapon_9mmAR", vecGunPos, vecGunAngles );
+					{
+						if( HasWeapon( HGRUNT_SHOTGUN ) )
+							DropItem( "weapon_shotgun_locked", vecGunPos, vecGunAngles );
+						else
+							DropItem( "weapon_mrc_locked", vecGunPos, vecGunAngles );
+					}
 
 					if (HasWeapon( HGRUNT_GRENADELAUNCHER ))
 						DropItem( "ammo_ARgrenades", BodyTarget( GetAbsOrigin() ), vecGunAngles );

server/monsters/monsters.h
@@ -58,6 +58,7 @@
 #define SF_MONSTER_DONTFORGET			BIT(13)	// diffusion - don't forget the enemy
 #define SF_MONSTER_ASLEEP				BIT(14) // diffusion - monster doesn't run its logic to save CPU (I use pre-spawned monsters on my maps and they are sitting idle in the box)
 #define	SF_MONSTER_NOPLAYERDAMAGE		BIT(15)	// diffusion - ignore damage from player only
+#define	SF_MONSTER_WEAPON_LOCKED		BIT(16) // diffusion - can't pickup dropped weapon	
 #define SF_MONSTER_FALL_TO_GROUND		0x80000000
 
 // specialty spawnflags

commit 53da21e90b444b3e817db8f79bd001897dd05f39

Fix critical bug introduced in b16d0b42a6504c32e11f512aadc9b98885f17381 (weapons didn't go through changelevel)

server/weapons/weapons.cpp
@@ -668,7 +668,7 @@ int CBasePlayerItem::ObjectCaps( void )
 	if( HasFlag( F_WEAPON_LOCKED ) )
 		return FCAP_IMPULSE_USE;
 
-	return 0;
+	return FCAP_ACROSS_TRANSITION;
 }
 
 //=========================================================